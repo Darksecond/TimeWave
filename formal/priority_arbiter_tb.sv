914ec3c3149b2fecc422cb037fddf31a0b4f346f
Tim Peters <tim@protonomaly.nl>
2022-11-27 14:28:03 +0100

    Add formal proof for priority_arbiter module

---
+++ formal/priority_arbiter_tb.sv
@@ -0,0 +1,41 @@
+`default_nettype none
+
+module priority_arbiter_tb
+#(
+  parameter Count
+)
+(
+  input wire logic clk_i,
+
+  input wire logic [Count-1:0] requests_i,
+  input wire logic [Count-1:0] grant_o,
+
+  input wire logic [Count-1:0] grant_q,
+  input wire logic [Count-1:0] lsb
+);
+
+logic past_valid;
+
+initial past_valid = 1'b0;
+
+always_ff @(posedge clk_i) past_valid <= 1'b1;
+
+always_ff @(posedge clk_i) begin
+  // We hold a previously held grant
+  if(past_valid && ((requests_i & $past(grant_o)) != '0)) begin
+    assert(grant_o == $past(grant_o));
+  end
+
+  // New grants are given based on priority
+  if(past_valid && ((requests_i & $past(grant_o)) == '0)) begin
+    casez(requests_i)
+      3'b000: assert(grant_o == 3'b000);
+      3'b??1: assert(grant_o == 3'b001);
+      3'b?10: assert(grant_o == 3'b010);
+      3'b100: assert(grant_o == 3'b100);
+      default: assert(0);
+    endcase
+  end
+end
+
+endmodule
--- rtl/priority_arbiter.sv
+++ rtl/priority_arbiter.sv
@@ -5,32 +5,33 @@ module priority_arbiter
   parameter Count = 0
 )
 (
-  input wire logic clk,
-  input wire logic reset_n,
+  input wire logic clk_i,
 
-  input wire logic [Count-1:0] requests,
-  output logic [Count-1:0] grant
+  input wire logic [Count-1:0] requests_i,
+  output logic [Count-1:0] grant_o
 );
 
-logic [Count-1:0] grant_reg;
+logic [Count-1:0] grant_q;
 logic [Count-1:0] lsb;
 
-assign lsb = requests & (-requests);
+assign lsb = requests_i & (-requests_i);
 
-initial begin
-  grant_reg = '0;
-end
+initial grant_q = '0;
 
 always_comb begin
-  grant = ((requests & grant_reg) != '0) ? grant_reg : lsb;
+  grant_o = ((requests_i & grant_q) != '0) ? grant_q : lsb;
 end
 
-always_ff @(posedge clk) begin
-  if(!reset_n) begin
-    grant_reg <= '0;
-  end else begin
-    grant_reg <= grant;
-  end
+always_ff @(posedge clk_i) begin
+  grant_q <= grant_o;
 end
+
+// Formal
+`ifdef FORMAL
+  priority_arbiter_tb
+  #(
+    .Count(Count)
+  ) tb0(.*);
+`endif
 
 endmodule
